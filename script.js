9e479dd8d26f827efb593dac13860fa28fe284ca
Rupkumar Sarkar <109466063+rup52333@users.noreply.github.com>
2024-02-17 20:56:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here
+const button=document.querySelector(".buton")
+const  input=document.querySelector(".input")
+let value=""
+input.addEventListener("input", 
+(e)=>{
+ value=e.target.value;
+}
+)
+button.addEventListener("click",  
+	()=>{
+     const item=document.createElement("li")
+      item.textContent=value
+     document.getElementById("list").appendChild(item)
+		
+	}
+	
+	)
 
